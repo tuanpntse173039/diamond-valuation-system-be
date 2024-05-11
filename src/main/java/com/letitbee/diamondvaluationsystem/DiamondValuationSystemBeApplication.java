b499d9b04ff970070904e37fb2f276b29d4a90ae
Tuan Pham <tuanpntse173039@fpt.edu.vn>
2024-05-11 15:08:35 +0700

    first commit - initial project

---
+++ src/main/java/com/letitbee/diamondvaluationsystem/DiamondValuationSystemBeApplication.java
@@ -0,0 +1,13 @@
+package com.letitbee.diamondvaluationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiamondValuationSystemBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiamondValuationSystemBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/letitbee/diamondvaluationsystem/DiamondValuationSystemBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.letitbee.diamondvaluationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiamondValuationSystemBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
