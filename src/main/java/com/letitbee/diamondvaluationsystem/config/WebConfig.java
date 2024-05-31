b7bb70a28cff9206028ed32eea2e93da5ab26625
Tuan Pham <tuanpntse173039@fpt.edu.vn>
2024-06-01 01:35:30 +0700

    added fe localhost CORS

--- src/main/java/com/letitbee/diamondvaluationsystem/config/WebConfig.java
+++ src/main/java/com/letitbee/diamondvaluationsystem/config/WebConfig.java
@@ -7,13 +7,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://hntdiamond.store", "console.hntdiamond.store")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000",
+                    "https://localhost:3000",
+                    "https://localhost:8080",
+                    "https://hntdiamond.store",
+                    "https://console.hntdiamond.store",
+                    "http://localhost:5173",
+                    "http://localhost:5174",
+                    "http://localhost:5175")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("*")
+            .allowCredentials(true);
+  }
 
 }
