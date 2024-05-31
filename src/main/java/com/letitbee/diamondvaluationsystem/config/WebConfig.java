228aae056ffdc8444967e3d781484cc3963b9117
Datdater <datnqse173081@fpt.edu.vn>
2024-05-31 10:25:14 +0700

    Merge remote-tracking branch 'origin/develop' into develop
    # Conflicts:
    #	src/main/java/com/letitbee/diamondvaluationsystem/config/WebConfig.java

--- src/main/java/com/letitbee/diamondvaluationsystem/config/WebConfig.java
+++ src/main/java/com/letitbee/diamondvaluationsystem/config/WebConfig.java
@@ -1,20 +1,19 @@
-package com.letitbee.diamondvaluationsystem.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                //.allowedOrigins("http://localhost:3000", "http://localhost:8080", "hntdiamond.store", "console.hntdiamond.store")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
-
-}
+//package com.letitbee.diamondvaluationsystem.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://hntdiamond.store", "console.hntdiamond.store")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE")
+//                .allowedHeaders("*")
+//                .allowCredentials(true);
+//    }
+//
+//}
