6c2a38b28cc5c0ea08ae1b1cb24682f5a9f8b1f1
Nguyen Tran <nguyentttse180432@fpt.edu.vn>
2024-06-16 12:07:37 +0700

    N1NJS1803-81: DiamondAssessment - Polish not be blank

--- src/test/java/com/letitbee/diamondvaluationsystem/DiamondAssessment.java
+++ src/test/java/com/letitbee/diamondvaluationsystem/DiamondAssessment.java
@@ -136,4 +136,26 @@ public class DiamondAssessment {
                         .content(accountJson))
                 .andExpect(status().isBadRequest());
     }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void testPolistBlank() throws Exception {
+        DiamondValuationNoteDTO diamondValuationNoteDTO = new DiamondValuationNoteDTO();
+        diamondValuationNoteDTO.setShape(Shape.valueOf("ROUND"));
+        diamondValuationNoteDTO.setCaratWeight(1.0F);
+        diamondValuationNoteDTO.setColor(Color.valueOf("D"));
+        diamondValuationNoteDTO.setClarity(Clarity.valueOf("IF"));
+        diamondValuationNoteDTO.setCut(Cut.valueOf("EXCELLENT"));
+        diamondValuationNoteDTO.setPolish(null);
+        diamondValuationNoteDTO.setSymmetry(Symmetry.valueOf("EXCELLENT"));
+        diamondValuationNoteDTO.setFluorescence(Fluorescence.valueOf("NONE"));
+
+        String accountJson = objectMapper.writeValueAsString(diamondValuationNoteDTO);
+
+        mockMvc.perform(put("/api/v1/diamond-valuation-notes/130")
+                        .contentType("application/json")
+                        .content(accountJson))
+                .andExpect(status().isBadRequest());
+    }
 }
