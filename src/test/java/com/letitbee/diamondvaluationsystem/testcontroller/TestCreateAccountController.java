516b6c430b2bb3e06f05cbb394e3a6923471108b
Datdater <datnqse173081@fpt.edu.vn>
2024-06-16 09:05:29 +0700

    add sample test

--- src/test/java/com/letitbee/diamondvaluationsystem/testcontroller/TestCreateAccountController.java
+++ src/test/java/com/letitbee/diamondvaluationsystem/testcontroller/TestCreateAccountController.java
@@ -1,61 +1,31 @@
 package com.letitbee.diamondvaluationsystem.testcontroller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.letitbee.diamondvaluationsystem.controller.AuthController;
 import com.letitbee.diamondvaluationsystem.enums.Role;
 import com.letitbee.diamondvaluationsystem.payload.AccountDTO;
-import com.letitbee.diamondvaluationsystem.security.JwtAuthenticationFilter;
-import com.letitbee.diamondvaluationsystem.security.JwtTokenProvider;
-import com.letitbee.diamondvaluationsystem.service.AccountService;
-import org.aspectj.lang.annotation.Before;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-@WebMvcTest(controllers = AuthController.class)
+@WithMockUser
+@SpringBootTest
+@AutoConfigureMockMvc
 public class TestCreateAccountController {
-
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private ObjectMapper objectMapper;
-
-    @MockBean
-    private AccountService accountService;
-
-    @MockBean
-    private AuthenticationManager authenticationManager;
-
-    @MockBean
-    private UserDetailsService customUserDetailsService;
-
-    @MockBean
-    private AuthController authController;
-
-    @MockBean
-    private JwtTokenProvider jwtTokenProvider;
-
-    @MockBean
-    private JwtAuthenticationFilter jwtAuthenticationFilter;
-    @BeforeEach
-    public void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
-
     @Test
+    @WithAnonymousUser
     public void testInvalidUsername() throws Exception {
         AccountDTO accountDTO = new AccountDTO();
         accountDTO.setUsername("da"); // Invalid username (too short)
@@ -71,10 +41,11 @@ public class TestCreateAccountController {
     }
 
     @Test
+    @WithMockUser
     public void testInvalidPassword() throws Exception {
         AccountDTO accountDTO = new AccountDTO();
-        accountDTO.setUsername("validUsername");
-        accountDTO.setPassword("123"); // Invalid password (too short)
+        accountDTO.setUsername("InvalidUsername");
+        accountDTO.setPassword("12345"); // Invalid password (too short)
         accountDTO.setRole(Role.CUSTOMER);
 
         String accountJson = objectMapper.writeValueAsString(accountDTO);
@@ -86,9 +57,11 @@ public class TestCreateAccountController {
     }
 
     @Test
+    @Transactional
+    @Rollback
     public void testValidAccount() throws Exception {
         AccountDTO accountDTO = new AccountDTO();
-        accountDTO.setUsername("validUsername");
+        accountDTO.setUsername("validUsername7");
         accountDTO.setPassword("validPassword123");
         accountDTO.setRole(Role.CUSTOMER);
 
@@ -98,15 +71,5 @@ public class TestCreateAccountController {
                         .contentType("application/json")
                         .content(accountJson))
                 .andExpect(status().isCreated());
-    }
-
-    @Autowired
-    private WebApplicationContext webApplicationContext;
-
-    @Before("setup")
-    public void setup()
-    {
-        //Init MockMvc Object and build
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
     }
 }
