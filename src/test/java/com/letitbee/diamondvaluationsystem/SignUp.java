498179e4631115bec6ebdf37259887414d3c1665
Datdater <datnqse173081@fpt.edu.vn>
2024-06-16 17:17:18 +0700

    N1NJS1803-38: Last name must not be blank

--- src/test/java/com/letitbee/diamondvaluationsystem/SignUp.java
+++ src/test/java/com/letitbee/diamondvaluationsystem/SignUp.java
@@ -121,5 +121,27 @@ public class SignUp {
                         .content(customerJson))
                 .andExpect(status().isBadRequest());
     }
+    @Test
+    @Transactional
+    @Rollback
+    public void testLastnameBlank() throws Exception {
+        CustomerDTO customerDTO = new CustomerDTO();
+        customerDTO.setFirstName("John");
+        customerDTO.setLastName(""); // Blank last name
+        customerDTO.setPhone("1234567890");
+        customerDTO.setAddress("testAddress");
+        customerDTO.setEmail("test@example.com");
+        customerDTO.setIdentityDocument("123456789012");
+        AccountResponse accountResponse = new AccountResponse();
+        accountResponse.setId(40L);
+        customerDTO.setAccount(accountResponse);
+
+        String customerJson = objectMapper.writeValueAsString(customerDTO);
+
+        mockMvc.perform(post("/api/v1/customers")
+                        .contentType("application/json")
+                        .content(customerJson))
+                .andExpect(status().isBadRequest());
+    }
 
 }
