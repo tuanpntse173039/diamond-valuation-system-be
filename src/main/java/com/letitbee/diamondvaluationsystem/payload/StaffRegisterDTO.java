1b5af0962e3cec9996a236ef8bf37c7c8d1be439
Datdater <datnqse173081@fpt.edu.vn>
2024-07-20 09:30:26 +0700

    remove pattern regex in staff register

--- src/main/java/com/letitbee/diamondvaluationsystem/payload/StaffRegisterDTO.java
+++ src/main/java/com/letitbee/diamondvaluationsystem/payload/StaffRegisterDTO.java
@@ -11,7 +11,6 @@ public class StaffRegisterDTO {
     @Size(min = 6, max = 24, message = "Username must be between 6 and 24 characters")
     private String username;
     @NotEmpty(message = "Password cannot be empty")
-    @Pattern(regexp = "^(?=.*[0-9]).{6,24}$", message = "Password must contain at least one numeric digit")
     @Size(min = 6, max = 24, message = "Password must be between 6 and 24 characters")
     private String password;
     @NotEmpty(message = "Email cannot be empty")
